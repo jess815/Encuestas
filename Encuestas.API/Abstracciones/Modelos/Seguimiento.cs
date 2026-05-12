17cb7ff03d149fcc4f698b3b1a205a5ad6b9bf89
Jessica Marín <jmarin@country.co.cr>
2026-05-12 16:26:23 -0600

    Crear Seguimiento

--- Encuestas.API/Abstracciones/Modelos/Seguimiento.cs
+++ Encuestas.API/Abstracciones/Modelos/Seguimiento.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Abstracciones.Modelos
 {
-    internal class Seguimiento
+    public class SeguimientoBase
     {
+        [Required(ErrorMessage = "El IdRespuesta es requerido")]
+        public int IdRespuesta { get; set; }
+
+        [Required(ErrorMessage = "El estado es requerido")]
+        [StringLength(30)]
+        public string Estado { get; set; } = string.Empty;
+    }
+
+    public class SeguimientoRequest : SeguimientoBase
+    {
+
+    }
+
+    public class SeguimientoResponse : SeguimientoBase
+    {
+        public int IdSeguimiento { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public DateTime? FechaResolucion { get; set; }
     }
 }
