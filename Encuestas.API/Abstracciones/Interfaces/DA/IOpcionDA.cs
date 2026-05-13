a2a57e971061b92f1277395bca82b42d4452d563
Jess <jmarin@country.co.cr>
2026-05-13 13:03:52 -0600

    configurar modulo de opcion

---
+++ Encuestas.API/Abstracciones/Interfaces/DA/IOpcionDA.cs
@@ -0,0 +1,17 @@
+﻿using Abstracciones.Modelos;
+
+namespace Abstracciones.Interfaces.DA
+{
+    public interface IOpcionDA
+    {
+        Task<IEnumerable<OpcionResponse>> Obtener();
+
+        Task<OpcionResponse> Obtener(int IdOpcion);
+
+        Task<int> Agregar(OpcionRequest opcion);
+
+        Task<int> Editar(int IdOpcion, OpcionRequest opcion);
+
+        Task<int> Eliminar(int IdOpcion);
+    }
+}
---
+++ Encuestas.API/Abstracciones/Modelos/Opcion.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Abstracciones.Modelos
+{
+    public class OpcionBase
+    {
+        [Required(ErrorMessage = "El texto es requerido")]
+        [StringLength(50)]
+        public string Texto { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El valor es requerido")]
+        public int Valor { get; set; }
+
+        [Required(ErrorMessage = "El orden visual es requerido")]
+        public int OrdenVisual { get; set; }
+
+        public bool Activo { get; set; }
+    }
+
+    public class OpcionRequest : OpcionBase
+    {
+
+    }
+
+    public class OpcionResponse : OpcionBase
+    {
+        public int IdOpcion { get; set; }
+    }
+}
